e3a98f3a2227d0476393aa6dd92dd94e4102c0e4
Taylor Hutchison <seanthutchison@gmail.com>
2015-12-25 15:14:41 -0600

    Added ability to do CORS requests with the --cors command line flag.

--- app.js
+++ app.js
@@ -23,6 +23,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+process.argv.forEach(function (val, index, array) {
+    if(val==='--cors'){
+        app.use(function(req,res,next){
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+            next();
+        });
+    }
+
+});
+
 app.use('/', routes);
 app.use('/api', api);
 app.use('/users', users);
